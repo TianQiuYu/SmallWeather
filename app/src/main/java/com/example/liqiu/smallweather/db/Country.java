55e58a728e7ca7602521fb8292aba9bf2b657fb6
wangliqiu <tianqiuyu96@gmail.com>
2018-09-26 09:08:09 +0800

    使用litepal创建数据库和表

---
+++ app/src/main/java/com/example/liqiu/smallweather/db/Country.java
@@ -0,0 +1,33 @@
+package com.example.liqiu.smallweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private int weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+}
